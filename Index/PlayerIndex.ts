c0d681d3e304b3e335f19122330c5825f60e6072
Nscoad <2199920564@qq.com>
2025-06-11 13:10:14 +0800

    玩家属性

---
+++ Index/PlayerIndex.ts
@@ -0,0 +1,15 @@
+/**
+ * PlayerAddressList: 定义玩家地址列表枚举
+ * PlayerDialogueList: 定义玩家对话列表枚举
+ */
+
+
+export enum PlayerAddressList{  //定义玩家地址列表枚举
+
+}   
+
+
+
+export enum PlayerDialogueList{ //定义玩家对话列表枚举
+
+}
