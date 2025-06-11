63a8440f323f26fa34a4a6df5e2082115d3e51e4
Nscoad <2199920564@qq.com>
2025-06-11 13:09:13 +0800

    共同属性

---
+++ Index/CommonIndex.ts
@@ -0,0 +1,33 @@
+/**
+ * DynastyList 朝代列表
+ * CompanyTypeListEnum公司类型列表枚举
+ * PlayerTypeList玩家属性列表枚举
+ * PlayerStatusList玩家生命值状态列表
+ * PlayerLevelList玩家等级列表
+ * RewardHpList玩家生命值状态列表
+ * 
+ */
+
+export enum DynastyList{ // 用于表示朝代列表
+ 
+}
+
+export enum CompanyTypeListEnum{ //  定义公司类型列表枚举
+ 
+}
+
+export enum PlayerTypeList{//  定义玩家属性列表枚举
+} 
+
+
+export enum PlayerStatusList{ //定义玩家生命值状态列表
+
+}
+
+export enum PlayerLevelList{ //定义玩家等级列表
+    
+}
+
+export enum RewardHpList{ //玩家生命值状态列表
+
+}
