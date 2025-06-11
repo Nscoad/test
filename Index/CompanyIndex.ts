759630b1f517ccc5bcbfffc930fbc028972156c0
Nscoad <2199920564@qq.com>
2025-06-11 13:09:50 +0800

    公司属性

---
+++ Index/CompanyIndex.ts
@@ -0,0 +1,21 @@
+/**
+ * CountryAddressEnum: 定义国家地址枚举
+ * CompanyAddressList: 定义公司地址列表枚举
+ * CompanyDialogueList: 定义公司hr对话列表枚举
+ */
+
+
+
+
+export enum CountryAddressEnum{ //  定义国家地址枚举
+
+}
+
+
+export enum  CompanyAddressList{ //  定义公司地址列表枚举
+
+}
+
+export enum CompanyDialogueList{ //  定义公司hr对话列表枚举
+
+}
