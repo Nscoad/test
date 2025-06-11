f0e4324e4996f8ef5257029a05d37538520d5403
Nscoad <2199920564@qq.com>
2025-06-11 13:29:48 +0800

    共同属性

--- Index/CommonIndex.ts
+++ Index/CommonIndex.ts
@@ -6,17 +6,26 @@
  * PlayerLevelList玩家等级列表
  * RewardHpList玩家生命值状态列表
  * 
+ * 
+ * 定义方式：export enum DynastyList{ // 用于表示朝代列表
+ *  qing = "秦",
+ *  h_an  = "汉",
+ *  Tang = "唐", 
+ *} 等号前必须是字母或下划线，不能是数字或其他符号
+ *  等号后可以为任何方式，包括中文、英文、数字等
+ * 
+ * 
  */
 
 export enum DynastyList{ // 用于表示朝代列表
+ q_ = "秦",
+}
+
+export enum CompanyTypeListEnum{ //  定义公司类型列表枚举
  
 }
 
-export enum CompanyTypeListEnum{ //  定义公司类型列表
- 
-}
-
-export enum PlayerTypeList{//  定义玩家属性列表
+export enum PlayerTypeList{//  定义玩家属性列表枚举
 } 
 
 
