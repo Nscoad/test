93e0e5679ea394ed51035a20277d4d2ed4b6bb17
Nscoad <2199920564@qq.com>
2025-06-11 13:31:14 +0800

    更新注释

--- Index/PlayerIndex.ts
+++ Index/PlayerIndex.ts
@@ -1,6 +1,22 @@
 /**
- * PlayerAddressList: 定义玩家地址列表枚举
- * PlayerDialogueList: 定义玩家对话列表枚举
+ * CountryAddressEnum: 定义国家地址枚举
+ * CompanyAddressList: 定义公司地址列表枚举
+ * CompanyDialogueList: 定义公司hr对话列表枚举
+ * 定义方式：export enum DynastyList{ // 用于表示朝代列表
+ *  qing = "秦",
+ *  h_an  = "汉",
+ *  Tang = "唐", 
+ *} 等号前必须是字母或下划线，不能是数字或其他符号
+ *  等号后可以为任何方式，包括中文、英文、数字等
+ * 
+ *  用于对话的枚举：
+ *  export enum CompanyDialogueList{ //  定义公司hr对话列表枚举
+    "你好，欢迎来到公司" 
+    "请问你有什么问题吗？" 
+    "我们公司有很多机会，你想了解哪些方面？"
+    "我们公司注重团队合作，你怎么看待团队合作？"
+    }
+    如果不需要特定选择就不需要等号，按照玩家点击来输出
  */
 
 
